8deffc29ae1a55fe6f4df9651aeb70cffa472fba
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-01-28 20:47:43 +0100

    integrate the template colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["*.html"],
   theme: {
+    colors:{
+      "transparent": "transparent",
+      "white": "#FFFFFF",
+      "black": "#000000",
+      "light-black": "#272727",
+      "dark-black": "#343332",
+      "light-gray": "#d3cfcf",
+      "dark-gray": "#6d6d6d",
+      "yellow": "#ffc000",
+      "orange": "#ff9800"
+    },
     extend: {},
   },
   plugins: [],
