ffefa58c0caea63bac67686f586026caf8f7f164
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-02-01 21:09:42 +0100

    config tailwind to handle js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["*.html", "assets/scripts/*.js"],
   theme: {
     colors:{
       "transparent": "transparent",
